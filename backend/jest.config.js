5f100d1589e890896b31d915dc35d5c2ee4f1c06
shubham9991 <68050359+shubham9991@users.noreply.github.com>
2025-10-06 01:03:44 +0530

    Delete backend directory

--- backend/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-  testMatch: ['**/tests/**/*.test.ts'],
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
-};
